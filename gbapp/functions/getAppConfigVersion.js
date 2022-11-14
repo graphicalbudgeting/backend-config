4323b648d8052b6dd88173768cc3a0f6c2b935c2
graphicalbudgeting <77788824+graphicalbudgeting@users.noreply.github.com>
2022-11-14 17:04:32 +0900

    Delete gbapp directory

--- gbapp/functions/getAppConfigVersion.js
+++
@@ -1,3 +0,0 @@
-exports = function(){
-  return context.values.get("appConfigVersion");
-};
--- gbapp/functions/validateReceipt.js
+++
@@ -1,9 +0,0 @@
-exports = async function(url, receiptData, password) {
-  const response = await context.http.post({
-    url: url,
-    body: {"receipt-data": receiptData, "password": password, "exclude-old-transactions": true},
-    encodeBodyAsJSON: true
-  })
-  // The response body is a BSON.Binary object. Parse it and return.
-  return EJSON.parse(response.body.text());
-};
